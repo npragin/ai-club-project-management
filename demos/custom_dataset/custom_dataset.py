dfd2575ecf3920293ecbc4ce265d04beabf235b7
npragin <npragin@gmail.com>
2025-11-05 14:59:07 -0800

    Lint custom dataset demo

--- demos/custom_dataset/custom_dataset.py
+++ demos/custom_dataset/custom_dataset.py
@@ -1,23 +1,29 @@
 import os
+from typing import Any, Callable, Optional
 
 import pandas as pd
-from torchvision.io import decode_image
+from torch import Tensor
 from torch.utils.data import Dataset
+from torchvision.io import decode_image
 
 
 class CustomImageDataset(Dataset):
     def __init__(
-        self, annotations_file, img_dir, transform=None, target_transform=None
-    ):
+        self,
+        annotations_file: str,
+        img_dir: str,
+        transform: Optional[Callable[[Tensor], Tensor]] = None,
+        target_transform: Optional[Callable[[Any], Any]] = None,
+    ) -> None:
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.img_labels)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> tuple[Tensor, Any]:
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
         image = decode_image(img_path)
         label = self.img_labels.iloc[idx, 1]
@@ -26,6 +32,7 @@ class CustomImageDataset(Dataset):
         if self.target_transform:
             label = self.target_transform(label)
         return image, label
+
 
 if __name__ == "__main__":
     dataset = CustomImageDataset("annotations.csv", ".")
